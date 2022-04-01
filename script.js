bfb9b153971871ab21dfa3ff6523c90803bd1f92
racimd <mracimdjenkal@gmail.com>
2022-04-01 23:19:20 +0100

    v1.11

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let y = document.getElementById("vente")
 
 let z = document.getElementById("gains")
 
+let w = document.getElementById("taxe")
+
 
 var form = document.getElementById("myform");
 function handleForm(event) { event.preventDefault(); } 
@@ -11,6 +13,7 @@ form.addEventListener('submit', handleForm);
 
 x[0].addEventListener("click",function(){
 	z.value = y.value * 0.95
+	w.value = y.value * 0.05
 
 
 
